6b426eb757e8a1c43b4f84434b70fc97bb7f0226
michu990902 <41807744+michu990902@users.noreply.github.com>
2020-07-26 00:24:08 +0200

    Custom validation and radio button field added

--- formik-test/src/components/Form/Form.js
+++ formik-test/src/components/Form/Form.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
+
+import styles from './Form.module.scss';
 
 import { connect } from 'react-redux';
-import { setFormData } from '../../redux/app/appActions'
+import { setFormData } from '../../redux/app/appActions';
 
 const Form = ({ setFormData }) => {
-    const [formValues, setFormValues] = useState({});
+    const [formValues, setFormValues] = useState({
+        email: "",
+        color: "#f00"
+    });
     return (
         <div>
             <Formik
-                initialValues={{...formValues}}
+                initialValues={formValues}
                 
                 onSubmit={values => console.log(values)}
+
+                validate={values => {
+                    const errors = {};
+                    if(!values.email){
+                        errors.email = "Required"
+                    }else if(
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ){
+                        errors.email = 'Invalid email address';
+                    }
+                    return errors;
+                }}
 
                 render={({
                     values,
@@ -25,10 +42,24 @@ const Form = ({ setFormData }) => {
                     <form onSubmit={handleSubmit}>
                         <input 
                             type="text" 
-                            name='content' 
-                            onChange={handleChange} 
-                            // value={values.content}
+                            name='email'
+                            placeholder="email" 
+                            onChange={handleChange}
+                            value={values.content}
                         />
+                        {errors.email && <div className={styles.error}>{errors.email}</div>}
+                        <Field component="div" name="color">
+                            <input
+                                type="radio"
+                                name="color"
+                                value="#fff"
+                            />
+                            <input
+                                type="radio"
+                                name="color"
+                                value="#000"
+                            />
+                        </Field>
                         <br/>
                         <input type="submit"/>
                     </form>
